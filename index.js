b073c2e19dad28227b45634adaeefbc95be338ef
mrhl082024 <mrhl082024@gmail.com>
2025-01-27 10:26:22 +0100

    succesfully created buttons for each mp3.file not sure if they play sound yet

--- index.js
+++ index.js
@@ -4,24 +4,29 @@ const audioArray = ["/home/laheromi/Desktop/vscode/javascript/soundboard/sounds/
 
 function createSoundBtn(){
     for (let i = 0; i < audioArray.length; i++) {
+        const soundCard = document.createElement("div");
+        soundCard.id = "sound-card";
         const title = document.createElement("p")
         title.textContent = "Sound";
         title.id = "title";
         const playBtn =document.createElement("button");
         playBtn.src = i;
-        playBtn.id = "play-btn"
+        playBtn.id = "play-btn";
+        playBtn.textContent = "Play";
         const pauseBtn = document.createElement("button");
         pauseBtn.src = i;
-        pauseBtn.id = "pause-btn"
+        pauseBtn.id = "pause-btn";
+        pauseBtn.textContent = "Pause";
         playBtn.addEventListener("click", () => {
 
         });
         pauseBtn.addEventListener("click", () => {
-            
+
         } );
-        container.appendChild(title)
-        container.appendChild(playBtn)
-        container.appendChild(pauseBtn)
+        soundCard.appendChild(title);
+        soundCard.appendChild(playBtn);
+        soundCard.appendChild(pauseBtn);
+        container.appendChild(soundCard)
     }
 }
 createSoundBtn()
