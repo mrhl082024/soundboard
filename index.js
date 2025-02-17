6517188e1baa1e7768cb31e2a2e6e55ff1e63e63
mrhl082024 <mrhl082024@gmail.com>
2025-02-17 10:37:28 +0100

    swapped away from homemade play and pause button, over to audio.controls. also added alot more sounds, styled some of it as well

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
 const container = document.querySelector("#container");
 
-const audioArray = ["sounds/rick_rolled_shortversion.mp3", "sounds/epic_sax_guy_shortversion.mp3"]
+const audioArray = [
+  "sounds/epic_sax_guy.mp3",
+  "sounds/fbi_open_up.mp3",
+  "sounds/metal_pipe.mp3",
+  "sounds/oh_baby_a_triple.mp3",
+  "sounds/quick_fart.mp3",
+  "sounds/rick_rolled.mp3",
+  "sounds/yooooo.mp3",
+];
 
-function createSoundBtn(){
-    for (let i = 0; i < audioArray.length; i++) {
-/*         const audio = new Audio([i]); */
-        const soundCard = document.createElement("div");
-        soundCard.id = "sound-card";
-        const audio = document.createElement("audio");
-        audio.src = audioArray[i];
-        const title = document.createElement("p");
-        title.textContent = "Sounds";
-        title.id = "title";
-        const playBtn =document.createElement("button");
-/*         playBtn.src = i; */
-        playBtn.id = "play-btn";
-        playBtn.textContent = "Play";
-        const pauseBtn = document.createElement("button");
-/*         pauseBtn.src = i; */
-        pauseBtn.id = "pause-btn";
-        pauseBtn.textContent = "Pause";
-        playBtn.addEventListener("click", () => {
-            audio.play();
-        });
-        pauseBtn.addEventListener("click", () => {
-            audio.pause();
-        } );
-        soundCard.appendChild(title);
-        soundCard.appendChild(playBtn);
-        soundCard.appendChild(pauseBtn);
-        container.appendChild(soundCard);
-        console.log();
-        
-        
-    }
+function createSoundBtn() {
+  for (let i = 0; i < audioArray.length; i++) {
+    const soundCard = document.createElement("div");
+    soundCard.id = "sound-card";
+
+    const audio = document.createElement(`audio`);
+    audio.src = audioArray[i];
+    audio.controls = true;
+    audio.id = "audio";
+
+    const title = document.createElement("p");
+    title.textContent = audioArray[i].split("sounds/").pop();
+    title.id = "title";
+
+    // const playBtn = document.createElement("button");
+    // playBtn.className = "sound-btn";
+    // playBtn.textContent = "Play";
+    // playBtn.id = "play-btn";
+
+    // const pauseBtn = document.createElement("button");
+    // pauseBtn.className = "sound-btn";
+    // pauseBtn.textContent = "Pause";
+    // pauseBtn.id = "pause-btn";
+
+    // playBtn.addEventListener("click", () => {
+    //   audio.play();
+    // });
+
+    // pauseBtn.addEventListener("click", () => {
+    //   audio.pause();
+    // });
+
+    soundCard.appendChild(title);
+    soundCard.appendChild(audio);
+    // soundCard.appendChild(playBtn);
+    // soundCard.appendChild(pauseBtn);
+    container.appendChild(soundCard);
+  }
 }
-createSoundBtn()
-
-const addSoundBtn = document.querySelector("#import-sound");
-const customContainer = document.getElementById("custom-sounds");
-
-let customSounds = [];
-
-addSoundBtn.addEventListener("click", () => {
-    const popUp = document.createElement("div");
-    const closeBtn = document.createElement("button");
-    const inputField = document.createElement("input");
-    const addBtn = document.createElement("button");
-    popUp.appendChild(closeBtn)
-    popUp.appendChild(inputField)
-    popUp.appendChild(addBtn)
-    window.open(popUp, "popup")
-})
-    
+createSoundBtn();
